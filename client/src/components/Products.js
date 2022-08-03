2501a8d37868d5875f4e71a47a65e951bed564b9
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-08-03 06:33:34 +0200

    First row of products page complete

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -6,6 +6,17 @@ const Products = () => {
         <>
         <div className="main-container-two">
             <div className="hor-line"></div>
+            <div className="product-info-container-left">
+                <div className="product-info-row-one">
+                <p className="product-one">Love Spell</p>
+                <p className="product-price">R90.00</p>
+                <p className="product-details">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Nec dignissim risus vitae et nisl a integer. Euismod sed justo neque non, nisl gravida placerat. 
+                 Sed laoreet integer cras venenatis aliquam nunc sed nunc viverra. Sed fermentum et, vitae magna 
+                 vitae fames dignissim urna tincidunt. Enim, porttitor purus in neque. Et pellentesque id eget 
+                 cursus. Turpis donec bibendum velit vel.</p>
+            </div>
+            </div>
         </div>
         </>
     )
