9e916cd33e488cfcfd1f9959a51ffe6e3dc622a3
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-08-03 12:51:33 +0200

    Renamed Shopping cart to checkout just for logic

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -6,12 +6,18 @@ const Checkout = () => {
         <>
         <div className="main-container-two">
             <div className="products-table-container">
-                <p className="shopping-cart-heading">Shopping Cart</p>
+                <p className="checkout-heading">Checking Out</p>
                 <div className="headings-container">
                 <p className="product-heading">Products</p>
                 <p className="size-heading">Size</p>
                 <p className="quantity-heading">Quantity</p>
                 <p className="total-price-heading">Total Price</p>
+                </div>
+                <div className="checkout-hor-line"></div>
+                <div className="checkout-content-container">
+                    <div className="row-one">
+                        <div className="checkout-product-img"></div>
+                    </div>
                 </div>
             </div>
             <div className="payment-info-container"></div>
