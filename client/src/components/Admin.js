8e21e2035cec82f8c7e99aebd711616236fc779d
Tsebo Rams <200200@virtualwindow.co.za>
2022-09-03 18:52:22 +0200

    Card Container added in Admin

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -8,20 +8,22 @@ const Admin = () => {
             <div className="admin-container">
             <p className="admin-heading">The Oilixate Shop: Oil Spells</p>
             </div>
-            <div className="card-one">
-                <div className="oil-img"></div>
-                <p className="oil-name">Lucid Dreaming Spell</p>
-                <div className="delete-btn"></div>
-                <p className="oil-size">Size:</p>
-                <p className="oil-colour-cap">Cap:</p>
-                <p className="stock-left">intock:</p>
-                <p className="oil-price">Price:</p>
-                <p className="oil-desc-label">Description:</p>
-                <p className="oil-desc">Lorem ipsum</p>
-                <p className="oil-disclaimer-label">Disclaimer:</p>
-                <p className="oil-disclaimer">Lorem ipsum</p>
-                <div className="view-btn"><p className="view-text">View product</p></div>
-                <div className="edit-btn"><p className="edit-text">Edit product</p></div>
+            <div className="card-container">
+                <div className="card-one">
+                    <div className="oil-img"></div>
+                    <p className="oil-name">Lucid Dreaming Spell</p>
+                    <div className="delete-btn"></div>
+                    <p className="oil-size">Size:</p>
+                    <p className="oil-colour-cap">Cap:</p>
+                    <p className="stock-left">intock:</p>
+                    <p className="oil-price">Price:</p>
+                    <p className="oil-desc-label">Description:</p>
+                    <p className="oil-desc">Lorem ipsum</p>
+                    <p className="oil-disclaimer-label">Disclaimer:</p>
+                    <p className="oil-disclaimer">Lorem ipsum</p>
+                    <div className="view-btn"><p className="view-text">View product</p></div>
+                    <div className="edit-btn"><p className="edit-text">Edit product</p></div>
+                </div>
             </div>
         </div>
         </>
