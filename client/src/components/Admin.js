902cec699cf736fb72728f858386be473f7c1265
Tsebo Rams <200200@virtualwindow.co.za>
2022-09-03 16:49:48 +0200

    Added div and p tags for the card content

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -5,7 +5,22 @@ const Admin = () => {
     return(
         <>
         <div className="main-container-three">
-            <div className=""></div>
+            <div className="admin-container">
+            <p className="admin-heading">The Oilixate Shop: Oil Spells</p>
+            </div>
+            <div className="card-one">
+                <div className="oil-img"></div>
+                <p className="oil-name">Lucid Dreaming Spell</p>
+                <div className="delete-btn"></div>
+                <p className="oil-size">Size:</p>
+                <p className="oil-colour-cap">Cap:</p>
+                <p className="stock-left">inStock:</p>
+                <p className="oil-price">Price:</p>
+                <p className="oil-desc-label">Description:</p>
+                <p className="oil-desc">Lorem ipsim</p>
+                <p className="oil-disclaimer-label">Disclaimer:</p>
+                <p className="oil-disclaimer">Lorem ipsum</p>
+            </div>
         </div>
         </>
     )
